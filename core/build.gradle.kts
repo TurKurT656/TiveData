e2a7ad68a5569c5d179fbde4968130acb0bfe87d
turkurt656 <turkurt656@gmail.com>
2020-04-21 17:59:13 +0430

    initialized core module

---
+++ core/build.gradle.kts
@@ -0,0 +1,34 @@
+plugins {
+    id(Plugins.Android.library)
+    kotlin(Plugins.Kotlin.android)
+    kotlin(Plugins.Kotlin.androidExtensions)
+}
+
+android {
+    compileSdkVersion(Config.compileSdkVersion)
+    buildToolsVersion = Config.buildToolsVersion
+
+    defaultConfig {
+        minSdkVersion(Config.minSdkVersion)
+        targetSdkVersion(Config.targetSdkVersion)
+        versionCode = Config.versionCode
+        versionName = Config.versionName
+
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+
+}
+
+dependencies {
+    implementation(Libraries.Core.kotlinStdlib)
+}
