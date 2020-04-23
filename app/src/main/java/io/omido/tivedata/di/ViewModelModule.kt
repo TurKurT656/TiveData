8d5db6b1c2a56db27a63638ea2e02595dd40a6f0
turkurt656 <turkurt656@gmail.com>
2020-04-23 14:22:34 +0430

    injected deps with di

--- app/src/main/java/io/omido/tivedata/di/ViewModelModule.kt
+++ app/src/main/java/io/omido/tivedata/di/ViewModelModule.kt
@@ -1,7 +1,13 @@
 package io.omido.tivedata.di
 
+import io.omido.tivedata.ui.home.HomeViewModel
+import io.omido.tivedata.ui.main.MainViewModel
+import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val viewModelModule = module {
+
+    viewModel { MainViewModel() }
+    viewModel { HomeViewModel() }
 
 }
