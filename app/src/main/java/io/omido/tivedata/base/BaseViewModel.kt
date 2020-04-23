edd467af34e2e32772ba49fa21d709b3671b3418
turkurt656 <turkurt656@gmail.com>
2020-04-23 13:16:05 +0430

    applied encapsulation to the events

--- app/src/main/java/io/omido/tivedata/base/BaseViewModel.kt
+++ app/src/main/java/io/omido/tivedata/base/BaseViewModel.kt
@@ -3,6 +3,7 @@ package io.omido.tivedata.base
 import android.net.Uri
 import android.widget.Toast
 import androidx.annotation.StringRes
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import androidx.navigation.NavDirections
 import com.google.android.material.snackbar.Snackbar
@@ -14,50 +15,51 @@ abstract class BaseViewModel : ViewModel() {
 
     /** Used in situations when we need to navigate to another fragment/activity
      */
-    val navigationCommand = LiveEvent<NavigationCommand>()
-
+    private val _navigationCommand = LiveEvent<NavigationCommand>()
+    val navigationCommand: LiveData<NavigationCommand> = _navigationCommand
 
     /**
      *      getting directions from the navigation's built actions
      */
     fun navigateTo(directions: NavDirections) {
-        navigationCommand.postValue(NavigationCommand.To(directions))
+        _navigationCommand.postValue(NavigationCommand.To(directions))
     }
 
     /**
      * using [NavController][androidx.navigation.NavController]'s navigateUp()
      */
     fun navigateBack() {
-        navigationCommand.postValue(NavigationCommand.Back)
+        _navigationCommand.postValue(NavigationCommand.Back)
     }
 
     /**
      * using [NavController][androidx.navigation.NavController]'s popBackStack()
      */
     fun navigateBackTo(destinationId: Int, inclusive: Boolean) {
-        navigationCommand.postValue(NavigationCommand.BackTo(destinationId, inclusive))
+        _navigationCommand.postValue(NavigationCommand.BackTo(destinationId, inclusive))
     }
 
-    val actionCommand = LiveEvent<ActionCommand>()
+    private val _actionCommand = LiveEvent<ActionCommand>()
+    val actionCommand: LiveData<ActionCommand> = _actionCommand
 
     fun openIntent(action: String? = null, type: String? = null, uri: Uri? = null) {
-        actionCommand.postValue(ActionCommand.OpenUrl(action, type, uri))
+        _actionCommand.postValue(ActionCommand.OpenUrl(action, type, uri))
     }
 
     fun showToast(message: String, duration: Int = Toast.LENGTH_SHORT) {
-        actionCommand.postValue(ActionCommand.ShowToast(message, duration))
+        _actionCommand.postValue(ActionCommand.ShowToast(message, duration))
     }
 
     fun showToast(@StringRes message: Int, duration: Int = Toast.LENGTH_SHORT) {
-        actionCommand.postValue(ActionCommand.ShowToastRes(message, duration))
+        _actionCommand.postValue(ActionCommand.ShowToastRes(message, duration))
     }
 
     fun showSnackBar(message: String, duration: Int = Snackbar.LENGTH_SHORT) {
-        actionCommand.postValue(ActionCommand.ShowSnackBar(message, duration))
+        _actionCommand.postValue(ActionCommand.ShowSnackBar(message, duration))
     }
 
     fun showSnackBar(@StringRes message: Int, duration: Int = Snackbar.LENGTH_SHORT) {
-        actionCommand.postValue(ActionCommand.ShowSnackBarRes(message, duration))
+        _actionCommand.postValue(ActionCommand.ShowSnackBarRes(message, duration))
     }
 
 }
